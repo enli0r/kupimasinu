ea2953266e709ef8b20068e5245ebc413377beb8
enli0r <milanniko19@gmail.com>
2022-10-14 15:39:49 +0200

    added logout form

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,16 @@
 <div class="navbar p-4 flex justify-end font-semibold gap-4">
     @auth
         <a href="{{ route('homepage') }}">Pocetna</a>
+
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+
+            <a href="route('logout')"
+                onclick="event.preventDefault();
+                this.closest('form').submit();">
+                Log out
+            </a>
+        </form>
     @endauth
 
     @guest
