39918f688da2ac0abc15453d319f7a7b4a819ae9
enli0r <milanniko19@gmail.com>
2022-10-30 01:23:03 +0200

    added filter animations

--- resources/views/components/sort.blade.php
+++ resources/views/components/sort.blade.php
@@ -4,7 +4,8 @@
 
     <button
         @click="showSort = !showSort"
-        class="bg-white flex justify-center items-center py-4 px-5 font-semibold rounded-xl"
+        @click.away="showSort=false"
+        class="bg-white gap-2 shadow-card flex justify-center items-center font-semibold rounded-xl px-4 py-3"
     >
         @if ($sortBy == 'cena' && $sortDirection == 'asc')
             Najjeftinije prvo
@@ -15,12 +16,23 @@
         @elseif ($sortBy == 'created_at' && $sortDirection == 'asc')
             Najstarije prvo
         @endif
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        </svg>
     </button>
 
     <div
     x-cloak
     x-show="showSort"
-    class="sort flex flex-col gap-2"
+    x-transition:enter="transition ease-out duration-150"
+    x-transition:enter-start="origin-top scale-y-0"
+    x-transition:enter-end="origin-top scale-y-100"
+    x-transition:leave="transition ease-out duration-300"
+    x-transition:leave-start="origin-top scale-y-100"
+    x-transition:leave-end="origin-top scale-y-0"
+    class="flex flex-col z-10 bg-white rounded-xl text-center py-2 mt-14 absolute left-0 top-0 w-full shadow-card sort"
+
     >
         <a wire:click.prevent="setOrderBy('cena', 'asc')" href="">Najjeftinije prvo</a>
         <a wire:click.prevent="setOrderBy('cena', 'desc')" href="">Najskuplje prvo</a>
--- resources/views/livewire/posts-index.blade.php
+++ resources/views/livewire/posts-index.blade.php
@@ -1,4 +1,4 @@
-<div class="space-y-7"
+<div class="space-y-7" style="min-height: 65vh;"
     x-data="{showFilters:false}"
     x-init="window.livewire.on('gotoTop', () => {
         window.scrollTo({
@@ -7,41 +7,91 @@
         })
     })"
 >
+    <!-- Filters -->
+    <div class="relative flex gap-5 md:hidden">
+        <div class="relative">
+            <x-sort :sortBy="$sort_by" :sortDirection="$sort_direction"/>
+        </div>
 
-    @if(count($posts) > 0)
 
-    <div class="flex justify-between gap-5 relative lg:flex-col-reverse">
         <button 
         @click="showFilters =! showFilters"
-        class="bg-white rounded-xl  px-5 shadow-card lgMin:hover:bg-gray-100 hover:transition-all w-1/2 lg:w-full lg:py-3 lg:px-5">
-            <div class="flex justify-between items-center">
+        @click.away="showFilters=false"
+        class="bg-white rounded-xl shadow-card px-4 py-3">
+            <div class="flex gap-3 justify-between items-center mdMin:w-40">
                 <p class="font-semibold">Filteri</p>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
             </div>
         </button>
 
+        
         <div
             x-cloak
             x-show="showFilters"
-            class="bg-white w-full p-5 shadow-card rounded-xl mt-5 filters"
+            x-transition:enter="transition ease-out duration-150"
+            x-transition:enter-start="origin-top scale-y-0"
+            x-transition:enter-end="origin-top scale-y-100"
+            x-transition:leave="transition ease-out duration-300"
+            x-transition:leave-start="origin-top scale-y-100"
+            x-transition:leave-end="origin-top scale-y-0"
+            class="bg-white w-full p-5 shadow-card rounded-xl mt-5 absolute left-0 top-9 z-10"
         >
             <x-filters />
         </div>
 
-        <div class="w-1/2 lg:w-full">
+        <div class="grow">
             <x-search />
+        </div>
+    </div>
+    <!-- End of filters -->
+
+    <!-- Phone filters -->
+    <div class="relative flex flex-col gap-5 mdMin:hidden">
+        <div class="flex gap-5">
+            <div class="relative grow shrink-0">
+                <x-sort :sortBy="$sort_by" :sortDirection="$sort_direction"/>
+            </div>
+            
+            <div class="grow">
+                <x-search />
+            </div>
         </div>
 
         <div class="relative">
-            <x-sort :sortBy="$sort_by" :sortDirection="$sort_direction"/>
+            <button 
+            @click="showFilters = !showFilters"
+            @click.away="showFilters=false"
+            class="bg-white rounded-xl shadow-card px-4 py-3 w-full">
+                <div class="flex gap-3 justify-between items-center">
+                    <p class="font-semibold">Filteri</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                    </svg>
+                </div>
+            </button>
+    
+            <div
+                x-cloak
+                x-show="showFilters"
+                x-transition:enter="transition ease-out duration-150"
+                x-transition:enter-start="origin-top scale-y-0"
+                x-transition:enter-end="origin-top scale-y-100"
+                x-transition:leave="transition ease-out duration-300"
+                x-transition:leave-start="origin-top scale-y-100"
+                x-transition:leave-end="origin-top scale-y-0"
+                class="bg-white w-full p-5 shadow-card rounded-xl absolute left-0 top-14 z-10"
+            >
+                <x-filters />
+            </div>
         </div>
-       
+        
+        
     </div>
+    <!-- End of phone filters -->
 
-    
-    
+    @if(count($posts) > 0)
 
     @foreach ($posts as $post)
 
