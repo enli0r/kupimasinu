d489beea0a40fe9a55047bb4009d8c379db43788
enli0r <milanniko19@gmail.com>
2022-11-05 09:28:13 +0100

    added logic from reuploading removed temp photos

--- app/Http/Livewire/ImagesCreate.php
+++ app/Http/Livewire/ImagesCreate.php
@@ -12,8 +12,9 @@ class ImagesCreate extends Component
     use WithFileUploads;
 
     public $images = [];
-    public $removeTempList = [];
+    // public $removeTempList = [];
     public $imagesToUpload = [];
+    public $imageNames = [];
 
     protected $listeners = ['saveImages'];
 
@@ -21,12 +22,38 @@ class ImagesCreate extends Component
 
 
     public function removeTemp($tempName){
-        array_push($this->removeTempList, $tempName);
+        //Removing images from imagesToUpload
+        foreach($this->imagesToUpload as $imgToUpload){
+            if($imgToUpload->getClientOriginalName() == $tempName){
+                $index = array_search($imgToUpload, $this->imagesToUpload);
+
+                unset($this->imagesToUpload[$index]);
+
+                $this->imagesToUpload = array_values($this->imagesToUpload);
+            }
+        }
+        
+        //Removing image from image names
+        $index = array_search($tempName, $this->imageNames);
+        unset($this->imageNames[$index]);
+        $this->imageNames = array_values($this->imageNames);
+
+        //Removing image from images
+        foreach($this->images as $img){
+            if($img->getClientOriginalName() == $tempName){
+                $index = array_search($img, $this->images);
+
+                unset($this->images[$index]);
+
+                $this->images = array_values($this->images);
+            }
+        }
     }
+
 
     public function saveImages($post_id, $user_id){
         if($this->validate()){
-            foreach($this->images as $image){
+            foreach($this->imagesToUpload as $image){
                 if(!(in_array($image->getFilename(), $this->removeTempList))){
                     $newName = $user_id.'-'.$post_id.'-'.$image->getClientOriginalName();
                     $image->storeAs('post-images', $newName);
@@ -45,11 +72,30 @@ class ImagesCreate extends Component
     }
 
 
+
     public function render()
     {
-        foreach($this->images as $image){
-            array_push($this->imagesToUpload, $image);
+
+        if(count($this->imagesToUpload) > 0){
+            foreach($this->images as $img){
+                if(!(in_array($img->getClientOriginalName(), $this->imageNames))){
+                    array_push($this->imagesToUpload, $img);
+                }
+            }
         }
+        
+        if(count($this->imagesToUpload) == 0){
+            foreach($this->images as $img){
+                array_push($this->imagesToUpload, $img);
+            }
+        }
+
+        foreach($this->imagesToUpload as $imgToUpload){
+            if(!(in_array($imgToUpload->getClientOriginalName(), $this->imageNames))){
+                array_push($this->imageNames, $imgToUpload->getClientOriginalName());
+            }
+        }
+
 
 
 
